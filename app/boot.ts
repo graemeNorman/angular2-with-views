3518291398e2bcedf8c22656b9508e1fb6cd37de
graemeNorman <graemenorman@tengroup.com>
2017-01-12 14:36:07 +0000

    First commit to repo && boot.ts file commented for future use

--- app/boot.ts
+++ app/boot.ts
@@ -1,10 +1,12 @@
-import 'core-js';
-import 'zone.js/dist/zone';
+import 'core-js';           // ES6 polyfil
+import 'zone.js/dist/zone'; // Angular uses this for change detection
 
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {ROUTER_PROVIDERS} from '@angular/router';
-import {AppComponent} from './app.component';
+// IMPORT COMPONENTS
+  import {bootstrap} from '@angular/platform-browser-dynamic';
+  import {ROUTER_PROVIDERS} from '@angular/router';
+  import {AppComponent} from './app.component';
 
-bootstrap(AppComponent, [
-  ROUTER_PROVIDERS
-]);
+// Instantiate our application and specify our root level component, AppComponent and inject ROUTER_PROVIDERS as a submodule
+  bootstrap(AppComponent, [
+    ROUTER_PROVIDERS
+  ]);
